d5a03fd1be49c36f89e45f7b92a6df3a307db3c3
淡月清云 <365326932@qq.com>
2016-11-05 14:51:35 +0800

    3.7注销用户、用户退出功能实现

---
+++ README.md
@@ -0,0 +1,35 @@
+# imooc movie used nodejs & mongodb
+Party from [uniquejava](https://github.com/uniquejava/node_imooc) Thank a lot!
+
+
+### Tech stack && node_modules
+* nodejs
+* express
+* jade
+* mongoose
+* moment
+* underscore
+* bower
+* jquery
+* bootstrap
+
+Developed with VS code under win10.
+
+### Install
+
+     npm install && bower install
+
+### Run
+Type `node app.js`
+
+|#| module|url|
+|---|---|---|
+|1|home|http://localhost:3000
+|2|add|http://localhost:3000/admin/movie
+|3|detail|http://localhost:3000/movie/:id
+|4|list|http://localhost:3000/admin/list
+|5|userlist|http://localhost:3000/admin/userlist
+
+### Todo (example)
+~~Fix warning~~ done(2016.2.26)
+> body-parser deprecated bodyParser: use individual json/urlencoded middlewares app.js:14:9
---
+++ app.js
@@ -0,0 +1,259 @@
+var express = require('express')
+var bodyParser = require('body-parser')
+var path = require('path')
+var session = require('express-session')
+var mongoose = require('mongoose')
+var mongoStore = require('connect-mongo')(session)
+var _ = require('underscore')
+var Movie = require('./models/movie')
+var User = require('./models/user')
+var port = process.env.PORT || 3000
+var app = express()
+var dbUrl = 'mongodb://localhost/imooc'
+
+mongoose.connect(dbUrl)
+
+app.set('views', './views/pages')
+app.set('view engine', 'jade')
+app.use(session({
+	secret: 'imooc',
+	store: new mongoStore({
+		url: dbUrl,
+		collection: 'sessions'
+	}),
+	resave: false,
+	saveUninitialized: true
+}))
+app.use(bodyParser.urlencoded({
+	extended: true
+}))
+app.use(express.static(path.join(__dirname, 'public')))
+app.locals.moment = require("moment")
+
+app.listen(port)
+
+
+console.log('immoc started on port ', port)
+
+// index page
+app.get('/', function  (req, res) {
+	console.log('user in session: ')
+	console.log(req.session.user)
+
+	var _user = req.session.user
+
+	if(_user) {
+		app.locals.user = _user
+	}
+
+	Movie.fetch(function(err, movies){
+		if(err){
+			console.log(err)
+		}
+
+		res.render("index",{
+			title: 'immoc home page',
+			movies: movies
+		})
+	})
+
+})
+
+//signup
+app.post('/user/signup', function(req, res) {
+	var _user = req.body.user
+
+	User.findOne({name: _user.name}, function(err, user) {
+		if (err) {
+			console.log(err)
+		}
+
+		if (user) {
+			return res.redirect('/')
+		}
+		else {
+			var user = new User(_user)
+
+			user.save(function(err, user) {
+				if(err) {
+					console.log(err)
+				}
+
+				res.redirect('/admin/userlist')
+			})
+		}
+	})	
+})
+
+//signin 
+app.post('/user/signin', function(req, res) {
+	var _user = req.body.user
+	var name = _user.name
+	var password = _user.password
+
+	User.findOne({name: name}, function(err, user) {
+		if(err) {
+			console.log(err)
+		}
+
+		if(!user) {
+			return res.redirect('/')
+		}
+
+		user.comparePassword(password, function(err, isMatch) {
+			if (err) {
+				console.log(err)
+			}
+
+			if(isMatch) {
+				req.session.user = user
+				return res.redirect('/')
+			}
+			else {
+				console.log('Password is not matched')
+			}
+		})
+	})
+})
+
+//logout
+app.get('/logout', function(req, res) {
+	delete req.session.user
+	delete app.locals.user
+
+	res.redirect('/')
+})
+
+// userlist page
+app.get('/admin/userlist', function  (req, res) {
+	User.fetch(function(err, users){
+		if(err){
+			console.log(err)
+		}
+
+		res.render("userlist",{
+			title: 'immoc 用户列表页',
+			users: users
+		})
+	})
+
+})
+
+// detail page
+app.get('/movie/:id', function  (req, res) {
+	var id = req.params.id
+	console.log("id is ", id)
+
+	Movie.findById(id, function(err, movie){
+		if(err){
+			console.log(err)
+		}
+		res.render("detail",{
+			title: 'imooc detail',
+			movie: movie
+		})
+	})
+})
+
+// admin page
+app.get('/admin/movie', function  (req, res) {
+	res.render("admin",{
+		title: 'immoc admin page',
+		movie: {
+			director: '',
+			country: '',
+			title: '',
+			year: '',
+			poster: '',
+			lang: '',
+			flash: '',
+			summary: ''
+		}
+	})
+})
+
+// admin update movie
+app.get('/admin/update/:id', function(req, res){
+	var id = req.params.id
+	console.log('updating ', id)
+
+	if(id){
+		Movie.findById(id, function(err,movie){
+			res.render('admin',{
+				title: 'imooc admin update page',
+				movie: movie
+			})
+		})
+	}
+})
+// admin post movie
+app.post('/admin/movie/new', function(req,res){
+	var id = req.body.movie._id
+	var movieObj = req.body.movie
+	var _movie
+	if(id !== 'undefined'){
+		Movie.findById(id, function(err, movie){
+			if(err){
+				console.log(err)
+			}
+
+			_movie = _.extend(movie, movieObj)
+			_movie.save(function(err, movie){
+				if(err){
+					console.log(err)
+				}
+
+				res.redirect('/movie/' + movie._id)
+			})
+		})
+	} else {
+		_movie = new Movie({
+			director: movieObj.director,
+			title: movieObj.title,
+			country: movieObj.country,
+			lang: movieObj.lang,
+			year: movieObj.year,
+			poster: movieObj.poster,
+			summary: movieObj.summary,
+			flash: movieObj.flash,
+		})
+		_movie.save(function(err, movie){
+			if(err){
+				console.log(err)
+			}
+
+			res.redirect('/movie/' + movie._id)
+		})
+	}
+})
+
+// index page
+app.get('/admin/list', function  (req, res) {
+	Movie.fetch(function(err, movies){
+		if(err){
+			console.log(err)
+		}
+
+		res.render("list",{
+			title: 'immoc list page',
+			movies: movies
+		})
+	})
+
+})
+
+app.delete('/admin/list',function(req,res){
+
+	var id = req.query.id
+	console.log("deleting ", id)
+
+	if(id){
+		Movie.remove(id, function(err,movie){
+			if(err){
+				console.log(err)
+			} else {
+				res.json({success: 1})
+			}
+		})
+	}
+})
